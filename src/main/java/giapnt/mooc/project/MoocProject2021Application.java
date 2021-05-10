4b225790ea7b4be635396e4e2dccdc777e0cff72
ntgpt <ntgptit@gmail.com>
2021-05-10 23:24:02 +0700

    first commit

---
+++ src/main/java/giapnt/mooc/project/MoocProject2021Application.java
@@ -0,0 +1,13 @@
+package giapnt.mooc.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocProject2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoocProject2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/giapnt/mooc/project/MoocProject2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package giapnt.mooc.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoocProject2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
